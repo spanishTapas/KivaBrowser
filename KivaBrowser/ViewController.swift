f2653047504af64a0daf150ce2e91032d2e981f7
Claire <dsbtargaryen@gmail.com>
2014-07-27 22:16:18 -0700

    Initial Commit

---
+++ KivaBrowser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KivaBrowser
+//
+//  Created by wanming zhang on 7/27/14.
+//  Copyright (c) 2014 wanming zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
